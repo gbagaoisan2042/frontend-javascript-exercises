484c6f7dabbbcadf6557fb51e32621220e0cd47c
Gilbert B <gbertb@gmail.com>
2014-06-15 21:37:50 +0000

    completed arrays challenge

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,11 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a, b, c, d) {
+  return [a, b, c, d]
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array) {
+
+  var lastItem = array.length -1
+
+  return [array[0], array[lastItem]]
+
+};
--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,24 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, array){
 
-module.exports.reverseSortedList = undefined;
+  if(array.indexOf(item)) {
+    array.push(item);
+    return array
+  }
+  else {
+    return array
+
+  }
+};
+
+module.exports.reverseSortedList = function(array){
+
+  array.sort();
+  array.reverse();
+
+   return array
+};
+
+
+// - Create a `reverseSortedList` function. This function should:
+//   - take one argument, an array
+//   - it should return the array sorted but in reverse
