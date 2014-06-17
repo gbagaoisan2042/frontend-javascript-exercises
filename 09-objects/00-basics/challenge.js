ede2f0156ac7077149bc935bbec60ecc879f2376
Gilbert B <gbertb@gmail.com>
2014-06-17 06:32:52 +0000

    attempt #1 for objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,53 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
 
-module.exports.addProperty = undefined;
+  return {title: courseTitle, duration: courseDuration, students: courseStudents}
 
-module.exports.formLetter = undefined;
+}
 
-module.exports.canIGet = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+
+  if (object.newProp) {
+    return object
+  }
+
+  else {
+    object[newProp] = newValue
+    return object
+  }
+
+}
+
+
+module.exports.formLetter = function(object) {
+
+   return "Hello " + object.recipient + ",\n\n" + object.msg + "\n\nSincerely,\n" + object.sender
+}
+
+module.exports.canIGet = function(item, money) {
+
+   var priceList = {};
+   priceList = { MacBook Air: 999, Macbook Pro: 1299, Mac Pro: 2499, Apple Sticker: 1 }
+
+}
+
+
+
+// - Complete the `canIGet` function. This function should:
+//     - Take two arguments:
+//         - `item`: represents what the user wants to buy
+//         - `money`: represents how many dollars a user has
+//     - return `true` if a user can afford a given item according to the price chart below, and false otherwise:
+//         - 'MacBook Air' - $999
+//         - MacBook Pro' - $1299
+//         - 'Mac Pro' - $2499
+//         - 'Apple Sticker' - $1
+//     - Return `false` if the `item` is not in the above list of Apple products
+
+//     > Do this with 0 'if' conditions! (Hint: Place the above price table in an object).
+
+//     For example:
+
+//     ```javascript
+//     canIGet('MacBook Air', 100) // returns false
+//     canIGet('MacBook Air', 1000) // returns true
+//     ```
