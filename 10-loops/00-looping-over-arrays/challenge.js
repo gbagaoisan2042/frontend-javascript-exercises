98839eaf03bf25d03ea25682c7e85e09a3c9bb12
Gilbert B <gbertb@gmail.com>
2014-06-18 06:53:26 +0000

    working on loops challenge

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,25 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
 
-module.exports.splitAndLowerCaseString = undefined;
+  for (var i = 0, sum = 0; i < array.length;sum += array[i++]);
+  return sum;
+}
+
+module.exports.splitAndLowerCaseString = function(string) {
+
+ var newArray = [];
+
+ var stringSplit = string.split(",");
+
+for (var i = 0; i < stringSplit.length; i++) {
+    newArray.push(stringSplit[i].toLowerCase());
+};
+
+return newArray;
+
+
+  // var splitArray = array.split(',');
+  // return lowerCase;
+}
+
 
 module.exports.addIndex = undefined;
